e6ff6315453ff5863b720c6ddcbe3b366f6854c9
Adil Bendaoui <90851562+Abendaoui@users.noreply.github.com>
2024-02-24 13:18:41 +0100

    add new func

--- contact.py
+++ contact.py
@@ -4,5 +4,6 @@ class Contact:
         self.name = name
         self.number = number
         self.email = email
-    
+    def __str__(self):
+        return f"{self.name}: {self.email}, {self.phone}"
     
